859f9d08b779414f858597d19f2ee15ac7c03258
osamamohti2002 <99078852@mydavinci.nl>
2024-04-18 09:06:48 +0200

    push

--- json_to_pdf.py
+++ json_to_pdf.py
@@ -2,13 +2,13 @@ import os
 import json
 
 # Importeer de functie die je hebt geschreven om facturen te genereren
-from je_eerste_pdf import generate_invoices
+from generate_invoices import generate_invoices
 
+oud_pad = 'JSON_IN'
+json_to_pdf_pad = 'INVOICE'
 # Pad naar de map met JSON-bestanden
-map_pad = 'test_set_softwareleverancier'
+map_pad = 'JSON_IN'
 aantal_pdf = len(os.listdir(map_pad))
-print(aantal_pdf)
-
 while aantal_pdf > 0:
 # Loop door alle bestanden in de map
     for bestandsnaam in os.listdir(map_pad):
@@ -22,4 +22,7 @@ while aantal_pdf > 0:
             factuur_naam = os.path.splitext(bestandsnaam)[0] + '_factuur.pdf'
             # Genereer de factuur met de gegevens uit het JSON-bestand en sla deze op onder de unieke naam
             generate_invoices(factuur_data, factuur_naam)
+            rsult = os.listdir(json_to_pdf_pad).append(bestandsnaam)
+            print(rsult)
+    os.rename(oud_pad, json_to_pdf_pad)
     aantal_pdf -= 1
